bc0d2863982178e7e21fba4f567ea5bd6c1d0452
daen <g.dani325@gmail.com>
2017-11-08 19:14:33 +0100

    Produced first drawing on board

--- src/gui/DrawingBoard.java
+++ src/gui/DrawingBoard.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
@@ -8,17 +10,30 @@ public class DrawingBoard extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	
-	private int brushSize = 20;
-	private Color brushColor = Color.BLACK;
+//	private int brushSize = 20;
+//	private Color brushColor = Color.BLACK;
+	private BufferedImage img;
 	
-	public void setBrushSize(int x){
-		brushSize = x;
+//	public void setBrushSize(int x){
+//		brushSize = x;
+//	}
+//	
+//	public void setBrushColor(Color c){
+//		brushColor = c;
+//	}
+	
+	public DrawingBoard(){
+		img = new BufferedImage(450, 450, BufferedImage.TYPE_INT_RGB);
 	}
 	
-	public void setBrushColor(Color c){
-		brushColor = c;
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		Graphics imageGraphics = img.getGraphics();
+		imageGraphics.setColor(Color.RED);
+		imageGraphics.fillRect(20, 20, 50, 100);
+		g.drawImage(img, 0, 0, null);
 	}
-	
 	
 
 }
