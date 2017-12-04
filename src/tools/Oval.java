8d715f16b6a59dcaeac9f80efdd85c29e58508fb
daen <g.dani325@gmail.com>
2017-12-04 23:16:03 +0100

    circle drawing fixed, working perfectly

--- src/tools/Oval.java
+++ src/tools/Oval.java
@@ -43,18 +43,18 @@ public class Oval extends PaintingDrawTool {
 		g.setColor(color);
 		int sizeX = Math.abs(e.getX() - startX);
 		int sizeY = Math.abs(e.getY() - startY);
-		startX = startX < e.getX() ? startX : e.getX();
-		startY = startY < e.getY() ? startY : e.getY();
+		int localStartX = startX < e.getX() ? startX : e.getX();
+		int localStartY = startY < e.getY() ? startY : e.getY();
 		if(size<4){
 			for(int i=0;i<size;++i)
-				g.drawOval(startX+i, startY+i, sizeX-2*i, sizeY-2*i);
+				g.drawOval(localStartX+i, localStartY+i, sizeX-2*i, sizeY-2*i);
 			return;
 		}
 		for(int i=0;i<size;++i){
-			g.drawOval(startX+i, startY+i, sizeX-2*i, sizeY-2*i);
-			g.drawOval(startX+i, startY+i-1, sizeX-2*i, sizeY-2*i-1);
-			g.drawOval(startX+i-1, startY+i, sizeX-2*i-1, sizeY-2*i);
-			g.drawOval(startX+i-1, startY+i-1, sizeX-2*i-1, sizeY-2*i-1);
+			g.drawOval(localStartX+i, localStartY+i, sizeX-2*i, sizeY-2*i);
+			g.drawOval(localStartX+i, localStartY+i-1, sizeX-2*i, sizeY-2*i-1);
+			g.drawOval(localStartX+i-1, localStartY+i, sizeX-2*i-1, sizeY-2*i);
+			g.drawOval(localStartX+i-1, localStartY+i-1, sizeX-2*i-1, sizeY-2*i-1);
 		}
 	}
 
